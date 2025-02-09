992df43c1f4d7527d3f69e29b162cd9a5b9e2591
Q <170919602+202420505@users.noreply.github.com>
2025-02-09 19:10:50 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,15 @@
 buildscript {
+    ext.kotlin_version = '1.9.10' // Or latest
     repositories {
-        google()  // ✅ Android Gradle Plugin 저장소
-        mavenCentral()  // ✅ Kotlin 플러그인 저장소
+        google()
+        mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+        classpath("com.android.tools.build:gradle:8.2.2") // Or latest
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
     }
 }
+
 
 allprojects {
     repositories {
