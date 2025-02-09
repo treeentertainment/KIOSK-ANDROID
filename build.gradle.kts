68fae61d86b539e1c408dfdca466e1107112bed2
Q <170919602+202420505@users.noreply.github.com>
2025-02-09 17:56:24 +0900

    Update and rename build.gradle to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         mavenCentral()  // ✅ Kotlin 플러그인 저장소
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.2.2'  // ✅ 최신 버전
-        classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0'
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
     }
 }
 
