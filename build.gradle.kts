0dff3ed2ab8b044d85c4fb52c37d03f640383e71
Q <170919602+202420505@users.noreply.github.com>
2025-02-09 19:44:35 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,8 @@
-import org.gradle.api.initialization.resolve.RepositoriesMode
-
-// gradle.properties에서 값 불러오기
-val agpVersion = project.properties["agpVersion"] as String? ?: "8.2.0"
-val kotlinVersion = project.properties["kotlinVersion"] as String? ?: "1.9.20"
-
-plugins {
-    id("com.android.application") version agpVersion apply false
-    id("com.android.library") version agpVersion apply false
-    id("org.jetbrains.kotlin.android") version kotlinVersion apply false
-}
-
 buildscript {
+    // gradle.properties에서 값 불러오기
+    val agpVersion = project.findProperty("agpVersion") as String? ?: "8.2.0"
+    val kotlinVersion = project.findProperty("kotlinVersion") as String? ?: "1.9.20"
+
     repositories {
         google()
         mavenCentral()
@@ -19,6 +11,13 @@ buildscript {
         classpath("com.android.tools.build:gradle:$agpVersion")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
+}
+
+plugins {
+    // plugins 블록에서는 변수를 사용할 수 없으므로 직접 버전 명시
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
 
 allprojects {
