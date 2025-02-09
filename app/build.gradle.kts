e0c9200ddd6e80a06d6ed03549391c1256b8492c
Q <170919602+202420505@users.noreply.github.com>
2025-02-09 19:01:52 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,20 @@ android {
             isMinifyEnabled = false
         }
     }
-}
+
+    // Add these blocks here:
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_17 // Or 1.8 if absolutely necessary
+        targetCompatibility JavaVersion.VERSION_17 // Or 1.8 if absolutely necessary
+        toolchain { // Recommended for Java 17+
+            languageVersion = JavaLanguageVersion.of(17) // Or 1.8
+        }
+    }
+    kotlinOptions {
+        jvmTarget = '17' // Or 1.8
+    }
+
+} // End of the android block
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
@@ -32,21 +45,17 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-
     implementation("io.appwrite:sdk-for-android:7.0.0")
-    
-    // 추가된 의존성 최신화
-    implementation("androidx.fragment:fragment-ktx:1.6.2")  
-    implementation("androidx.recyclerview:recyclerview:1.3.1") 
 
-    // ViewPager 및 CoordinatorLayout 최신화
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
+
     implementation("androidx.viewpager2:viewpager2:1.1.0")
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
 
-    // OkHttp BOM 제거하고 개별 라이브러리 명시
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")  
-    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0") 
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
 
-    // 불필요한 legacy 지원 제거 고려
-    // implementation("androidx.legacy:legacy-support-core-utils:1.0.0") 
+    // Consider removing if not needed:
+    // implementation("androidx.legacy:legacy-support-core-utils:1.1.0.0")
 }
