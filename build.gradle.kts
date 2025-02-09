991ac0a9b7d0f704665d97924c279c48fdf7c137
Q <170919602+202420505@users.noreply.github.com>
2025-02-09 19:24:01 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 // build.gradle.kts (Project-level)
 
 plugins {
-    // If you have any plugins here, keep them.  Otherwise, this block can be omitted.
+    id 'com.android.application' version '8.2.0' apply false
+    id 'com.android.library' version '8.2.0' apply false
+    id 'org.jetbrains.kotlin.android' version '2.1.10' apply false
 }
 
 allprojects {
@@ -17,8 +19,11 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2") // Or latest
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${project.property("kotlinVersion")}")
+        val kotlinVersion: String by project // Get from gradle.properties
+        val agpVersion: String by project      // Get from gradle.properties
+
+        classpath("com.android.tools.build:gradle:$agpVersion")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
 
