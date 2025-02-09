9ce7992c51fb72ccf6a4fd4b85df8c9bd5060291
Q <170919602+202420505@users.noreply.github.com>
2025-02-09 19:15:18 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,8 @@
-buildscript {
-    ext.kotlin_version = '1.9.10' // Or latest
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2") // Or latest
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-    }
-}
+// build.gradle.kts (Project-level)
 
+plugins {
+    // If you have any plugins here, keep them.  Otherwise, this block can be omitted.
+}
 
 allprojects {
     repositories {
@@ -18,7 +11,19 @@ allprojects {
     }
 }
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+val kotlinVersion = "1.9.10" // Or latest version
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2") // Or latest
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
 }
 
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
+}
