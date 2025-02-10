a3224e547d45cf1e309b834987baf28f19208c92
Q <170919602+202420505@users.noreply.github.com>
2025-02-10 20:28:57 +0900

    Update NewWebActivity.kt

--- app/src/main/java/me/moontree/treekiosk/v3/NewWebActivity.kt
+++ app/src/main/java/me/moontree/treekiosk/v3/NewWebActivity.kt
@@ -14,6 +14,7 @@ import io.appwrite.Query
 import kotlinx.coroutines.launch
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import android.content.Intent
 
 class NewWebActivity : AppCompatActivity() {
 
