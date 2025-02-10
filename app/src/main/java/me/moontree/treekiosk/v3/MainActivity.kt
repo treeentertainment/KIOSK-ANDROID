8016bf32d70419529a2703b17e839ec6df0c34b2
Q <170919602+202420505@users.noreply.github.com>
2025-02-11 00:49:53 +0900

    Update MainActivity.kt

--- app/src/main/java/me/moontree/treekiosk/v3/MainActivity.kt
+++ app/src/main/java/me/moontree/treekiosk/v3/MainActivity.kt
@@ -18,9 +18,6 @@ import java.io.InputStreamReader
 import android.content.Intent
 import android.content.DialogInterface
 import androidx.appcompat.app.AlertDialog
-import android.content.BroadcastReceiver
-import android.content.IntentFilter
-import android.content.Context
 
 class MainActivity : AppCompatActivity() {
 
@@ -295,23 +292,4 @@ webView.webChromeClient = object : WebChromeClient() {
             "{}" // 에러 발생 시 빈 JSON 반환
         }
     }
-    private val messageReceiver = object : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
-        val message = intent?.getStringExtra("message") ?: return
-        webView.evaluateJavascript(
-            "window.dispatchEvent(new MessageEvent('message', { data: '$message' }));",
-            null
-        )
-    }
-}
-
-override fun onResume() {
-    super.onResume()
-    registerReceiver(messageReceiver, IntentFilter("NEW_WEB_MESSAGE"))
-}
-
-override fun onPause() {
-    super.onPause()
-    unregisterReceiver(messageReceiver)
-}
 }
