c5c9566afab272829a3793e9d6255141c393586d
Q <170919602+202420505@users.noreply.github.com>
2025-02-11 22:25:43 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,10 +38,10 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file("keystore.jks")
-            storePassword = System.getenv("890890890")
-            keyAlias = System.getenv("treekiosk")
-            keyPassword = System.getenv("890890890")
+            storeFile = file("/keystore.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
         }
     }
 
