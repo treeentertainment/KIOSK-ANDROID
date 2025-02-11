4e6c600b232a5ba216cc848a219bb96c150461bd
Q <170919602+202420505@users.noreply.github.com>
2025-02-11 11:41:11 +0900

    Update MainActivity.kt

--- app/src/main/java/me/moontree/treekiosk/v3/MainActivity.kt
+++ app/src/main/java/me/moontree/treekiosk/v3/MainActivity.kt
@@ -292,4 +292,24 @@ webView.webChromeClient = object : WebChromeClient() {
             "{}" // 에러 발생 시 빈 JSON 반환
         }
     }
+
+        @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (intent != null && intent.hasExtra("webMessage")) {
+            String message = intent.getStringExtra("webMessage");
+            Log.d("MainActivity", "Received message from NewActivity: " + message);
+            
+            // WebView1로 메시지 전달
+            sendMessageToWebView(webView1, message);
+        }
+    }
+
+    private void sendMessageToWebView(WebView webView, String message) {
+        webView.evaluateJavascript("receiveMessage('" + message + "')", null);
+    }
 }
