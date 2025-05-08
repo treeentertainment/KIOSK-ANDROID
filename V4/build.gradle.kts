3a455a230c1ff358af872affab3d068e6c2c182a
Q <170919602+202420505@users.noreply.github.com>
2025-05-08 14:56:13 +0900

    Update build.gradle.kts

--- V4/build.gradle.kts
+++ V4/build.gradle.kts
@@ -5,11 +5,5 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-        classpath("com.google.gms:google-services:4.4.0")
     }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
 }
