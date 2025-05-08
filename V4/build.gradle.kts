8d9026f0f73329b0be38f85e4d8ea4587176ab74
Q <170919602+202420505@users.noreply.github.com>
2025-05-08 15:57:44 +0900

    Update build.gradle.kts

--- V4/build.gradle.kts
+++ V4/build.gradle.kts
@@ -1,32 +1,6 @@
-buildscript {
-    // gradle.properties에서 값 불러오기
-    val agpVersion = project.findProperty("agpVersion") as String? ?: "8.2.0"
-    val kotlinVersion = project.findProperty("kotlinVersion") as String? ?: "1.9.20"
-
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:$agpVersion")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-    }
-}
-
 plugins {
-    // plugins 블록에서는 변수를 사용할 수 없으므로 직접 버전 명시
-    id("com.android.application") version "8.2.0" apply false
-    id("com.android.library") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
