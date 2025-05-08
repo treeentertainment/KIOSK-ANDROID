34ddc6bb48a17e262c80b7639adb27d2db09d26f
moonlight <160602348+treecorporation@users.noreply.github.com>
2025-05-08 20:17:50 +0900

    Update build.gradle.kts

--- V4/app/build.gradle.kts
+++ V4/app/build.gradle.kts
@@ -30,6 +30,8 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+    composeOptions {
         kotlinCompilerExtensionVersion = "1.5.3"
     }
     kotlinOptions {
