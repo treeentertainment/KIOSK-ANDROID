9b1ec8f4155a730a43ceb794100f3eb279218144
Q <170919602+202420505@users.noreply.github.com>
2025-05-08 15:57:31 +0900

    Update settings.gradle.kts

--- V4/settings.gradle.kts
+++ V4/settings.gradle.kts
@@ -1,11 +1,24 @@
-// settings.gradle.kts
-
 pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
 
-rootProject.name = "TREE KIOSK V4"  // Change to your project name
-include(":app")  // Include other modules if necessary
+rootProject.name = "TREE KIOSK V4"
+include(":app")
+ 
