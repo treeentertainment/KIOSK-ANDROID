d9aed58c38c93a608c8944780213e0f225b03fae
Q <170919602+202420505@users.noreply.github.com>
2025-05-08 14:42:57 +0900

    Update build.gradle.kts

--- V4/build.gradle.kts
+++ V4/build.gradle.kts
@@ -1,20 +1,23 @@
-buildscript {
+// Root-level build.gradle.kts
+
+plugins {
+    // 플러그인은 여기에 작성하지 않음 (보통 module-level에 사용)
+}
+
+dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
     }
-    dependencies {
-        // Android Gradle Plugin 버전 명시
-        classpath("com.android.tools.build:gradle:8.2.0")
-        // Kotlin Gradle Plugin
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-
-        // 필요 시 추가적인 classpath 의존성
-        // classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
-    }
 }
 
-allprojects {
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        // classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
+    }
+
     repositories {
         google()
         mavenCentral()
