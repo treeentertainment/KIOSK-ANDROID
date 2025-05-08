cc82901ff1cd9f95b4f9ff9c1818e76b7a2a9461
Q <170919602+202420505@users.noreply.github.com>
2025-05-08 15:17:07 +0900

    Update build.gradle.kts

--- V4/app/build.gradle.kts
+++ V4/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.2.0"
-    id("org.jetbrains.kotlin.android") version "1.9.22"
+    id("org.jetbrains.kotlin.android") version "1.9.20"
     id("org.jetbrains.kotlin.plugin.compose") version "1.9.22"
     id("com.google.gms.google-services") version "4.4.0"
 }
