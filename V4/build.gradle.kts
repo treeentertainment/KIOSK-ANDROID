3f042af72ca3784621e107e954c1968f1650b02f
Q <170919602+202420505@users.noreply.github.com>
2025-05-08 14:39:36 +0900

    Update build.gradle.kts

--- V4/build.gradle.kts
+++ V4/build.gradle.kts
@@ -1,6 +1,26 @@
-plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-    id("com.google.gms.google-services")
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Android Gradle Plugin 버전 명시
+        classpath("com.android.tools.build:gradle:8.2.0")
+        // Kotlin Gradle Plugin
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+
+        // 필요 시 추가적인 classpath 의존성
+        // classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
