3b10701b774c174d73be44a778a83bc6261c3e36
Q <170919602+202420505@users.noreply.github.com>
2025-05-08 14:45:57 +0900

    Update build.gradle.kts

--- V4/build.gradle.kts
+++ V4/build.gradle.kts
@@ -1,20 +1,12 @@
-dependencyResolutionManagement {
+buildscript {
     repositories {
         google()
         mavenCentral()
     }
-}
-
-buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-        // classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
-    }
-
-    repositories {
-        google()
-        mavenCentral()
+        classpath("com.google.gms:google-services:4.4.0")
     }
 }
 
